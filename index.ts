eee07323fa534eccfd27597226b345353f598c62
Juhana Jauhiainen <juhana.jauhiainen@gmail.com>
2021-11-25 08:57:42 +0200

    final example

--- index.ts
+++ index.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
-
 dotenv.config();
-// rest of the code remains same
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
 const app = express();
 const PORT = 8000;
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  return res.json(users);
 });
